fc5a38e9258e073b460c0cddc36f17b92c8674a1
Birgit <S1610238029@fhooe.at>
2018-05-14 19:05:19 +0200

    counter

--- www/js/numbercounter.js
+++ www/js/numbercounter.js
@@ -1,52 +1,98 @@
-const Counter = {
-	settings: {
-		speed: 1000
-	},
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
 
-	count: function(number_el, number) {
-		const _this = this;
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
 
-		let speed = _this.settings.speed;
-		if (number_el.hasAttribute("data-speed")) {
-			speed = parseInt(number_el.getAttribute("data-speed"));
-		}
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
 
-		if (number_el && number > 0) {
-			const step_min = 50;
-			let step_speed = Math.abs(Math.floor(speed / number));
-			    step_speed = Math.max(step_speed, step_min);
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
 
-		    const start_time = new Date().getTime();
-		    const end_time = start_time + speed;
-		    let timer;
+			$self.data('countTo', data);
 
-		    const step = () => {
-		    	const now = new Date().getTime();
-		    	const left = Math.max((end_time - now) / speed, 0);
-		    	const value = Math.round(number - (left * number));
-		    	number_el.innerHTML = value;
-		    	if (value == number)
-		    		clearInterval(timer);
-		    };
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
 
-		    timer = setInterval(step, step_speed);
-		    step();
-		}
-	},
+			// initialize the element with the starting value
+			render(value);
 
-	init: function(el) {
-		const _this = this;
-		const number_el = el.getElementsByClassName("counter-number")[0];
-		const number = parseInt(number_el.innerText);
-		number_el.innerHTML = "0";
-		_this.count(number_el, number);
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+
+				render(value);
+
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
 	}
-};
+}(jQuery));
 
-(function() {
-	// Initiate all instances on the page
-	const counters = document.getElementsByClassName("counter");
-	for (let i = 0; i < counters.length; i++) {
-		Counter.init(counters[i]);
+jQuery(function ($) {
+  // custom formatting example
+  $('.count-number').data('countToOptions', {
+	formatter: function (value, options) {
+	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
 	}
-})();
+  });
+
+  // start all the timers
+  $('.timer').each(count);
+
+  function count(options) {
+	var $this = $(this);
+	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+	$this.countTo(options);
+  }
+});
