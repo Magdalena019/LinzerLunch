264c3515f50ed932df1015467e4c01302c73f899
Birgit <S1610238029@fhooe.at>
2018-06-06 11:38:30 +0200

    counter dynamically

--- www/js/main.js
+++ www/js/main.js
@@ -4,4 +4,57 @@ $(document).ready(function() {
   $('#textHits').addClass('hide');
   $('#headline').addClass('hide');
 
+/*  $.getJSON("algolia/counterValues.json", function(json){
+    console.log(json);
+    var items=[];
+    $.each(json, function(key,val){
+      //items.push(val);
+      console.log(val.restaurant);
+      $('#firstCounter').attr("data-count", val.restaurant);
+      $('#secondCounter').attr("data-count", val.cafe);
+      $('#thirdCounter').attr("data-count", val.bar);
+
+    });
+
+    //console.log(items);
+    //$('#firstCounter').attr("data-count", items[0]);
+    //$('#secondCounter').attr("data-count", items[1]);
+    //$('#thirdCounter').attr("data-count", items[2]);
+
+});*/
+
+var requestURL="algolia/counterValues.json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType='json';
+request.send();
+
+request.onload= function(){
+  var json= request.response;
+  $.each(json, function(key,val){
+    //items.push(val);
+    console.log(val.restaurant);
+    $('#firstCounter').attr("data-count", val.restaurant);
+    $('#secondCounter').attr("data-count", val.cafe);
+    $('#thirdCounter').attr("data-count", val.bar);
+
+  });
+}
 });
+
+/*$.ajax({
+  dataType: "json";
+  url: "algolia/counterValues.json";
+  mimeType: "application/json";
+  success: function(result){
+    $.each(json, function(key,val){
+      //items.push(val);
+      console.log(val.restaurant);
+      $('#firstCounter').attr("data-count", val.restaurant);
+      $('#secondCounter').attr("data-count", val.cafe);
+      $('#thirdCounter').attr("data-count", val.bar);
+
+    });
+  }
+
+});*/
