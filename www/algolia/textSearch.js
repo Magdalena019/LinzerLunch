8aae5672215a266bc7325472860d3c4e29b8244f
nataschara <s1610238066@fhooe.at>
2018-06-16 23:44:11 +0200

    VolltextSuche2

--- www/algolia/textSearch.js
+++ www/algolia/textSearch.js
@@ -27,15 +27,16 @@ $(document).ready(function() {
 
         var button = $();
 
-        var $hit = $('<em><br></em>' +
-            '<div class="hit">' + hit.name + '</div>' +
-            '<div class="resultInfo hide" value="' + hit.name + '">' +
-            '<img src="../www/restaurants/' + hit.path + '/01.jpg" class="rounded mx-auto d-block">' +
-            '<p><b><br>' + hit.name + '</b></p>' +
-            '<p>' + hit.description + '</p>' +
-            '<p>Info: ' + hit.info + '</p>' +
-            '</div>' +
-            '<button class="getInfo hide" value="' + hit.name + '">Zurück</button>');
+          var $hit = $('<em><br></em>' +
+              '<div class="hit">' + hit.name + '</div>' +
+              '<div class="resultInfo hide" value="' + hit.name + '">' +
+              '<img src="../www/restaurants/' + hit.path + '/01.jpg" class="rounded thumbImage">' +
+              '<p><b><br>' + hit.name + '</b></p>' +
+              '<p>' + hit.description + '</p>' +
+              '<p>Info: ' + hit.info + '</p>' +
+              '<p>Öffnungszeiten: ' + hit.hours + '</p>' +
+              '</div>' +
+              '<button class="getInfo hide" value="' + hit.name + '">Zurück</button>');
 
           $hit.click(function() {
           var name = hit['name'];
