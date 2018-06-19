5aa3a6500bf73242e79325d0999b68a32cadbc38
Birgit <S1610238029@fhooe.at>
2018-06-19 13:51:05 +0200

    random hits

--- www/algolia/getRandom.js
+++ www/algolia/getRandom.js
@@ -19,7 +19,23 @@ function random() {
 
       success: function(data) {
         //TODO: hier dem template die werte zuweisen
-        random.text(data["name"] + " " + data["info"]);
+        /*random.text(data["name"] + " " + data["info"]);*/
+        random.html('<em><br></em>' +
+            '<div class="hit">' + data["name"] + '</div>' +
+            '<div class="resultInfo" value="' + data["name"] + '">' +
+            '<img src="../www/restaurants/' + data["path"] + '/01.jpg" class="rounded thumbImage">' +
+            '<h4><b><br>' + data["name"] + '</b></h4>' +
+            '<p>' + data["description"] + '</p>' +
+            '<p>Info: ' + data["info"] + '</p>' +
+            '<h5>Öffnungszeiten: ' + '</h5>' +
+            '<p>Montag: ' + data["hours"]["mon"] + '<br>' +
+            'Dienstag: ' + data["hours"]["tue"] + '<br>' +
+            'Mittwoch: ' + data["hours"]["wed"] + '<br>' +
+            'Donnerstag: ' + data["hours"]["thu"] + '<br>' +
+            'Freitag: ' + data["hours"]["fri"] + '<br>' +
+            'Samstag: ' + data["hours"]["sat"] + '<br>' +
+            'Sonntag: ' + data["hours"]["sun"]+ '</p>' +
+            '</div>')
       }
     });
   }
